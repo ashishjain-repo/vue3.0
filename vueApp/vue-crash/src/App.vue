a13bbd737120f34fd014d617a700c1c05635753f
Ashish Jain <ashishjain99@yahoo.com>
2024-10-31 12:39:55 -0600

    Added Vue app understood directives

---
+++ vueApp/vue-crash/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default{
+  data(){
+    return{
+      name: 'John Doe',
+      status: 'pending',
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is Active</p>
+  <p v-else-if="status === 'pending'">User is Pending</p>
+  <p v-else>User is In-Active</p>
+</template>
