06fbe8042d9aa02ad70c1464611439f66ddb906d
Ashish Jain <ashishjain99@yahoo.com>
2024-10-31 17:43:41 -0600

    Added loop directive

--- vueApp/vue-crash/src/App.vue
+++ vueApp/vue-crash/src/App.vue
@@ -4,6 +4,7 @@ export default{
     return{
       name: 'John Doe',
       status: 'pending',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
     };
   },
 };
@@ -14,4 +15,9 @@ export default{
   <p v-if="status === 'active'">User is Active</p>
   <p v-else-if="status === 'pending'">User is Pending</p>
   <p v-else>User is In-Active</p>
+
+  <h3>Tasks: </h3>
+<ul>
+  <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
 </template>
